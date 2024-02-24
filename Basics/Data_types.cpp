23d93f07f548d04ab61c06222ba9c67d67460025
bunnysayzz <stfuazzo@gmail.com>
2024-02-24 13:37:15 +0530

    24th feb

---
+++ Basics/Data_types.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    long y;
+    double z;
+    string a;
+    char b;
+    cin >> x >> y >> z >> a >> b;
+    cout << x << y << z << a << b;
+    return 0;
+}
