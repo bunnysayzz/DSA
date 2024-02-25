2c2bb4b0460f9f3ac4feabb709f824ec7ab7c634
bunnysayzz <stfuazzo@gmail.com>
2024-02-25 10:17:35 +0530

    25th Feb

---
+++ CBasics/ifelse.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int x;
+    cin >> x;
+    if(x >= 20){
+        cout << "This number is grater than 20";
+    }
+    else {
+        cout << "This number is smaller than 20";
+    }
+    return 0;
+}
